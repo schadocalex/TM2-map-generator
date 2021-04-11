ca58c85fa9780a091855ea1c4aa6a003a0ff0ee1
Alexis Schad <schadoc_alex@hotmail.fr>
2021-04-11 19:57:59 +0200

    new generation algorithm with true fallback in case of collisions

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const { startBot } = require('./src/bot');
 
 startBot({
-  blocks_between_checkpoints: [100, 100],
+  blocks_between_checkpoints: [6, 12],
+  nb_checkpoints: [2, 4],
   start_pos_x: [6, 10],
   start_pos_y: [0, 10],
   start_pos_z: [6, 10]
