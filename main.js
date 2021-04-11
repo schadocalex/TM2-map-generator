1172fc25b43fdbacfe0d5a0c19abbe4f6916dd40
JohanZasada <zasada.johan@gmail.com>
2021-04-11 19:19:36 +0200

    add start scripts and process args setup

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const { startBot } = require('./src/bot');
+const argv = require('minimist')(process.argv.slice(2));
 
 startBot({
-  blocks_between_checkpoints: [100, 100],
+  blocks_between_checkpoints: [argv.min ? argv.min : 100, argv.max ? argv.max : 100],
   start_pos_x: [6, 10],
   start_pos_y: [0, 10],
   start_pos_z: [6, 10]
