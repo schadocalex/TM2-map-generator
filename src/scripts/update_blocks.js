a9ab1c7256004e583c93b74e75817200ee19dc81
JohanZasada <zasada.johan@gmail.com>
2021-04-12 19:11:09 +0200

    add inputs and outputs frequency to update blocks script

--- src/scripts/update_blocks.js
+++ src/scripts/update_blocks.js
@@ -3,7 +3,16 @@ const { blocks_data } = require('../data/blocks_data');
 
 blocks_data.forEach(block => {
   console.log(block);
+  // Block frequency
   const newBlock = Object.assign({ frequency: 1 }, block);
+  // inputs frequency
+  newBlock.inputs.forEach((input, key) => {
+    newBlock.inputs[key] = Object.assign({ frequency: 1 }, input);
+  });
+  // outputs frequency
+  newBlock.outputs.forEach((output, key) => {
+    newBlock.outputs[key] = Object.assign({ frequency: 1 }, output);
+  });
   console.log(newBlock);
   // fs.writeFileSync(`../data/${bloc.keys.join('')}.json`, JSON.stringify(newBlock), 'utf8');
 });
